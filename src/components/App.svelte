de65ff7bd65cc8fadbaf8b36e02e27b8b98cc263
acai1031 <cys021031@163.com>
2024-02-13 16:30:07 -0800

    first try

---
+++ src/components/App.svelte
@@ -0,0 +1,55 @@
+<script>
+  import { onMount } from 'svelte';
+  import * as d3 from 'd3';
+  import Internet from './Internet.svelte';
+
+  let data = [];
+
+  onMount(async () => {
+    const res = await fetch('../static/Internet_usage_data.csv'); 
+    const csv = await res.text();
+    data = d3.csvParse(csv, d3.autoType);
+    console.log(data.length);
+  });
+</script>
+
+<main>
+  <h1>Internet Usage Project</h1>
+  <Internet {data} />
+</main>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap');
+
+  :root {
+    --color-bg: #ffffff;
+    --color-outline: #c2c2c2;
+    --color-shadow: hsl(0, 0%, 0%, 0.1);
+    --color-text: #3f4252;
+    --color-bg-1: hsla(0, 0%, 0%, 0.2);
+    --color-shadow-1: hsl(0, 0%, 96%);
+  }
+
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  main {
+    text-align: center;
+    font-family: 'Nunito', sans-serif;
+    font-weight: 300;
+    line-height: 2;
+    font-size: 24px;
+    color: var(--color-text);
+  }
+
+  h1 {
+    font-size: 2em;
+    font-weight: 300;
+    line-height: 2;
+  }
+</style>
---
+++ src/components/Internet.svelte
@@ -0,0 +1,32 @@
+<script>
+  import * as d3 from 'd3';
+
+  export let data;
+
+  const width = 928;
+  const height = 600;
+  const marginTop = 20;
+  const marginRight = 30;
+  const marginBottom = 30;
+  const marginLeft = 40;
+
+  $: console.log(data[0]);
+</script>
+
+<div class="internet-plot">
+  <svg
+    {width}
+    {height}
+    viewBox="0 0 {width} {height}"
+    style="max-width: 100%; height: auto;"
+  >
+  <g>
+  </g>
+  </svg>
+</div>
+
+<!--
+Sources:
+https://observablehq.com/@d3/global-temperature-trends?intent=fork
+https://www.nytimes.com/interactive/2017/01/18/science/earth/2016-hottest-year-on-record.html
+-->
